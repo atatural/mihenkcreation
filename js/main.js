bd596564fb3091681c8141999ed2b5de31e3de08
atatural <ataberktural@galatasaray.net>
2021-05-04 16:15:00 +0300

    16:14 04.05.2021

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("annen");
+
+var mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
+mySong.play();
+
+icon.onclick = function(){
+    if(mySong.paused){
+        mySong.play()
+        mySong.volume=0.07;
+        icon.src= "img/pause.png"
+    }
+    else{
+        mySong.pause()
+        icon.src= "img/play.png"
+    }
+}
